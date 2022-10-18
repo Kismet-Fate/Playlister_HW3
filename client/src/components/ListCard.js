1f94524ee17e7c59dc7d5724c5c95a0aa2a96838
Kismet-Fate <yoloswagftw545@gmail.com>
2022-10-18 16:52:17 -0400

    added delete list

--- client/src/components/ListCard.js
+++ client/src/components/ListCard.js
@@ -1,6 +1,6 @@
-import { useContext, useState } from 'react'
-import { useHistory } from 'react-router-dom'
-import { GlobalStoreContext } from '../store'
+import { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { GlobalStoreContext } from '../store';
 /*
     This is a card in our list of playlists. It lets select
     a list for editing and it has controls for changing its 
@@ -10,16 +10,27 @@ import { GlobalStoreContext } from '../store'
 */
 function ListCard(props) {
     const { store } = useContext(GlobalStoreContext);
-    const [ editActive, setEditActive ] = useState(false);
-    const [ text, setText ] = useState("");
+    const [editActive, setEditActive] = useState(false);
+    const [text, setText] = useState('');
     store.history = useHistory();
     const { idNamePair, selected } = props;
+
+    const handleDeleteList = (event) => {
+        event.stopPropagation();
+        let id = event.target.id.substring('delete-list-'.length);
+        const showDeleteListModal = () => {
+            let modal = document.getElementById('delete-list-modal');
+            modal.classList.add('is-visible');
+        };
+        store.selectListToDelete(id);
+        showDeleteListModal();
+    };
 
     function handleLoadList(event) {
         if (!event.target.disabled) {
             let _id = event.target.id;
             if (_id.indexOf('list-card-text-') >= 0)
-                _id = ("" + _id).substring("list-card-text-".length);
+                _id = ('' + _id).substring('list-card-text-'.length);
 
             // CHANGE THE CURRENT LIST
             store.setCurrentList(_id);
@@ -34,73 +45,76 @@ function ListCard(props) {
     function toggleEdit() {
         let newActive = !editActive;
         if (newActive) {
-            store.setIsListNameEditActive();
+            store.setListNameActive();
         }
         setEditActive(newActive);
     }
 
     function handleKeyPress(event) {
-        if (event.code === "Enter") {
-            let id = event.target.id.substring("list-".length);
+        if (event.code === 'Enter') {
+            let id = event.target.id.substring('list-'.length);
             store.changeListName(id, text);
             toggleEdit();
         }
     }
     function handleUpdateText(event) {
-        setText(event.target.value );
+        setText(event.target.value);
     }
 
-    let selectClass = "unselected-list-card";
+    let selectClass = 'unselected-list-card';
     if (selected) {
-        selectClass = "selected-list-card";
+        selectClass = 'selected-list-card';
     }
     let cardStatus = false;
     if (store.isListNameEditActive) {
         cardStatus = true;
     }
-    let cardElement =
+    let cardElement = (
         <div
             id={idNamePair._id}
             key={idNamePair._id}
             onClick={handleLoadList}
-            className={'list-card ' + selectClass}>
+            className={'list-card ' + selectClass}
+        >
             <span
-                id={"list-card-text-" + idNamePair._id}
-                key={"span-" + idNamePair._id}
-                className="list-card-text">
+                id={'list-card-text-' + idNamePair._id}
+                key={'span-' + idNamePair._id}
+                className='list-card-text'
+            >
                 {idNamePair.name}
             </span>
             <input
                 disabled={cardStatus}
-                type="button"
-                id={"delete-list-" + idNamePair._id}
-                className="list-card-button"
-                value={"\u2715"}
+                type='button'
+                id={'delete-list-' + idNamePair._id}
+                className='list-card-button'
+                onClick={handleDeleteList}
+                value={'🗑'}
             />
             <input
                 disabled={cardStatus}
-                type="button"
-                id={"edit-list-" + idNamePair._id}
-                className="list-card-button"
+                type='button'
+                id={'edit-list-' + idNamePair._id}
+                className='list-card-button'
                 onClick={handleToggleEdit}
-                value={"\u270E"}
+                value={'\u270E'}
             />
-        </div>;
+        </div>
+    );
 
     if (editActive) {
-        cardElement =
+        cardElement = (
             <input
-                id={"list-" + idNamePair._id}
+                id={'list-' + idNamePair._id}
                 className='list-card'
                 type='text'
                 onKeyPress={handleKeyPress}
                 onChange={handleUpdateText}
                 defaultValue={idNamePair.name}
-            />;
+            />
+        );
     }
-    return (
-        cardElement
-    );
+    return cardElement;
 }
 
 export default ListCard;
